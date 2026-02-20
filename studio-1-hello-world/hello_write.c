03e96ec2536a88db545540c0a76fa4e182f57963
chennethelius <97802838+chennethelius@users.noreply.github.com>
2026-02-19 22:02:08 -0700

    finished homework

--- studio-1-hello-world/hello_write.c
+++ studio-1-hello-world/hello_write.c
@@ -1,1 +1,14 @@
-//Replace the contents of this file with your hello_write.c
+// Max Chen
+// 02/18/2026
+// Prints a Hello World message using the write() system call
+
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+
+    char message[] = "Hello, world!\n";
+
+    write(STDOUT_FILENO, message, 14);
+
+    return 0;
+}
